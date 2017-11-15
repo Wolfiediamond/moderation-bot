d78c3dfedead42b028ae2049f7bbd8a56a15699c
Wolfiediamond <wolfiediamond@gmail.com>
2017-11-15 19:37:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -299,4 +299,4 @@ client.on("message", (message) => {
   }
 });
 
-client.login(process.env.MzgwMDUyMjE3NjMxMzQyNjI1.DOy-uw.Ienej_m5UzPQulCwbPBYrlfCMJQ);
+client.login("MzgwMDUyMjE3NjMxMzQyNjI1.DOy-uw.Ienej_m5UzPQulCwbPBYrlfCMJQ");
